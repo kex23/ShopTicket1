aa4d72f0a10fc5a538c91bb155495e95e4cc636a
Kiady <rabenjatovokex@gmail.com>
2024-11-20 13:08:24 +0300

    API

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://https://shop-ticket.vercel.app/api'
+  ? 'https://shop-ticket.vercel.app/api/reactions'
   : 'http://localhost:3000/api';
 
 export default API_URL;
